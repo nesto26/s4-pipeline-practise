00d3f76857f303d7dfc98f9d554c258229149f20
nesto26 <97629942+nesto26@users.noreply.github.com>
2022-12-21 09:16:54 -0600

    Update check.sh

--- check.sh
+++ check.sh
@@ -3,12 +3,12 @@
 USER=$User
 
 cat permission.txt | grep -i $USER 
-
-if 
-[[ $? -eq 0 ]]
-thgen
-echo "You have permission to run this job"
-else
-echo "You don't have permission to run this job"
-exit 1
-fi
+echo $?
+# if 
+# [[ $? -eq 0 ]]
+#then
+#echo "You have permission to run this job"
+#else
+#echo "You don't have permission to run this job"
+#exit 1
+#fi
